37cf800e673d2b39ae04ade3255c39ca1f9c71e2
aidanmrli <aidan.li@mail.utoronto.ca>
2024-09-17 19:22:58 +0200

    normalize data in preprocessing

--- data/process_graphs4mer_normalized.py
+++ data/process_graphs4mer_normalized.py
@@ -1,4 +1,3 @@
-# This script processes the EEG data and labels and creates a PyTorch Geometric dataset.
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -10,7 +9,7 @@ import os
 
 # Load adjacency matrix
 adj = sio.loadmat('/home/data_shared/adj_mat.mat')['adj_mat']
-print(adj[0:5,0:5])
+# print(adj[0:5,0:5])
 # Create edge index and edge weight from adjacency matrix
 ind = (adj != 0) & (adj != 1)
 edge_index = np.argwhere(ind == True).T 
@@ -22,13 +21,6 @@ for i, e in enumerate(edge_index.T):
 edge_index = torch.tensor(edge_index, dtype=torch.long)
 edge_weight = torch.tensor(edge_weight, dtype=torch.float)
 
-# This graph basically has 36 undirected/bidirectional edges in the graph because each undirected edge has two directed edges.
-# There are 19 electrodes and 72 directional edges in the graph. 
-# Every directional edge has its opposite edge in the graph with the same weight.
-# The edge index is a 2x72 tensor, where each column represents an edge between two electrodes.
-# The edge weight is a 1x72 tensor, where each element represents the weight of the corresponding edge.
-# The adjacency matrix is a 19x19 tensor, where each element represents the weight of the edge between two electrodes.
-
 # Load electrode positions
 pos = sio.loadmat('/home/data_shared/position.mat')['pos'].T
 
@@ -39,7 +31,7 @@ with h5py.File('/home/data_shared/clip_data.h5', "r") as f:
 with h5py.File('/home/data_shared/label.h5', "r") as f: 
     Label = np.array(f[list(f.keys())[0]])
 
-# NOTE: EVAL Load EEG data and labels 
+# Load EEG data and labels for evaluation
 with h5py.File('/home/data_shared/clip_data_eval.h5', "r") as f:
     EEG_eval = np.array(f[list(f.keys())[0]])
 
@@ -50,79 +42,79 @@ assert EEG.shape[0] == Label.shape[0], "Data and label size mismatch"
 assert EEG.shape[1] == 10, "Number of time points mismatch"
 assert EEG.shape[2] == 19, "Number of electrodes mismatch"
 assert EEG.shape[3] == 200, "Number of time bins in each time point mismatch"
+# (B, L=10, V=19, D=200) -> (B, V, L=2000, D=1) 
+# 1 scalar feature per node or 3 for rgb
 
 # Prepare the dataset
 dataset = []
-# batch = np.ones((1,19))
-is_fft = True
+
+# Calculate mean and std from training data
+train_data = EEG.transpose(0, 2, 1, 3).reshape(-1, 19, 2000)
+mean_train = np.mean(train_data, axis=2, keepdims=True)
+std_train = np.std(train_data, axis=2, keepdims=True)
+print("Mean_train.shape: ", mean_train.shape)
+print("Std_train.shape: ", std_train.shape)
 
 def normalize_data(data, mean, std):
     return (data - mean) / (std + 1e-10)
 
 for idx in tqdm(range(EEG.shape[0])):
     eeg_clip = EEG[idx,:,:,:]
-    
-    if is_fft:
-        eeg_clip = np.log(np.abs(fft(eeg_clip,axis=2)[:,:,0:100]) + 1e-30)
-    # should be (10, 19, 100)
-    # normalize over frequency axis
-    mean = np.mean(eeg_clip, axis=2, keepdims=True)
-    std = np.std(eeg_clip, axis=2, keepdims=True)
-    eeg_clip = normalize_data(eeg_clip, mean, std)
-    eeg_clip = eeg_clip.reshape(19, 10, 100)
-
+    eeg_clip = eeg_clip.transpose(1, 0, 2).reshape(19, -1)
+    eeg_clip = normalize_data(eeg_clip, mean_train[idx, :, :], std_train[idx, :, :])
+    eeg_clip = eeg_clip[:, :, np.newaxis]
     label = Label[idx]
+    
     dataset.append(Data(
-        x=torch.tensor(eeg_clip, dtype=torch.float32),  # .transpose(1,0)
+        x=torch.tensor(eeg_clip, dtype=torch.float32),
         y=torch.tensor(label, dtype=torch.float32),
-        # batch=batch,
         edge_weight=edge_weight.squeeze(0),    
         edge_index=edge_index,
         elec_pos=torch.tensor(pos)
     ))
 
-# Split the dataset into train, validation sets
-train_size = int(0.9 * len(dataset))
-val_size = len(dataset) - train_size
-
+# Prepare the test dataset
+# Calculate mean and std from test data
+test_data = EEG_eval.transpose(0, 2, 1, 3).reshape(-1, 19, 2000)
+mean_test = np.mean(test_data, axis=2, keepdims=True)
+std_test = np.std(test_data, axis=2, keepdims=True)
+print("Mean_test.shape: ", mean_test.shape)
+print("Std_test.shape: ", std_test.shape)
 test_dataset = []
 
 for idx in tqdm(range(EEG_eval.shape[0])):
     eeg_clip = EEG_eval[idx,:,:,:]
+    eeg_clip = eeg_clip.transpose(1, 0, 2).reshape(19, -1)
     
-    if is_fft:
-        eeg_clip = np.log(np.abs(fft(eeg_clip,axis=2)[:,:,0:100]) + 1e-30)
+    # Normalize the data using test set statistics
+    eeg_clip = normalize_data(eeg_clip, mean_test[idx, :, :], std_test[idx, :, :])
     
-    # normalize over frequency axis
-    mean = np.mean(eeg_clip, axis=2, keepdims=True)
-    std = np.std(eeg_clip, axis=2, keepdims=True)
-    eeg_clip = normalize_data(eeg_clip, mean, std)
-    eeg_clip = eeg_clip.reshape(19, 10, 100)
+    eeg_clip = eeg_clip[:, :, np.newaxis]
     label = Label_eval[idx]
 
     test_dataset.append(Data(
         x=torch.tensor(eeg_clip, dtype=torch.float32),
         y=torch.tensor(label, dtype=torch.float32),
-        # batch=batch,
         edge_weight=edge_weight.squeeze(0),    
         edge_index=edge_index,
         elec_pos=torch.tensor(pos)
     ))
 
+# Split the dataset into train and validation sets
+train_size = int(0.9 * len(dataset))
+val_size = len(dataset) - train_size
 train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
 
+# Save the datasets
 script_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(script_dir)
 home_dir = os.path.dirname(parent_dir)
 save_dir = os.path.join(home_dir, 'processed_dataset')
 os.makedirs(save_dir, exist_ok=True)
-if is_fft:
-    torch.save(train_dataset, os.path.join(save_dir, 'train_dataset_fft.pt'))
-    torch.save(val_dataset, os.path.join(save_dir, 'val_dataset_fft.pt'))
-    torch.save(test_dataset, os.path.join(save_dir, 'test_dataset_fft.pt'))
-else:
-    torch.save(train_dataset, os.path.join(save_dir, 'train_dataset_raw.pt'))
-    torch.save(val_dataset, os.path.join(save_dir, 'val_dataset_raw.pt'))
-    torch.save(test_dataset, os.path.join(save_dir, 'test_dataset_raw.pt'))
 
-print(f"Datasets saved in {save_dir}")
+
+torch.save(train_dataset, os.path.join(save_dir, 'train_dataset_raw_s4mer_normalized.pt'))
+torch.save(val_dataset, os.path.join(save_dir, 'val_dataset_raw_s4mer_normalized.pt'))
+torch.save(test_dataset, os.path.join(save_dir, 'test_dataset_raw_s4mer_normalized.pt'))
+
+print(f"Normalized datasets saved in {save_dir}")
