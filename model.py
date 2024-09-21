9c9955c0398d3810431096644befe79a166fc13e
aidanmrli <aidan.li@mail.utoronto.ca>
2024-09-21 19:03:30 +0200

    remove warmup

--- model.py
+++ model.py
@@ -4,13 +4,13 @@ import torch.nn.functional as F
 from torch_geometric.data import Data
 import torch.optim as optim
 from mamba_ssm import TGMamba
-from torchmetrics import Accuracy, F1Score, AUROC
+from torchmetrics import Accuracy, F1Score, AUROC, CohenKappa
 from torch.optim.lr_scheduler import LinearLR, CosineAnnealingLR, CosineAnnealingWarmRestarts, SequentialLR
 
 
 class LightGTMamba(L.LightningModule):
     def __init__(self, 
-                 num_vertices=19, 
+                 dataset='tuhz',
                  conv_type='graphconv', 
                  seq_pool_type='last', 
                  vertex_pool_type='mean',
@@ -31,7 +31,22 @@ class LightGTMamba(L.LightningModule):
                  pass_edges_to_next_layer=False,
                  **kwargs):
         super().__init__()
-        self.num_vertices = num_vertices
+        torch.set_float32_matmul_precision('high') 
+
+        self.dataset = dataset
+        if dataset == 'tuhz':
+            num_classes = 1
+            self.num_vertices = 19
+            self.accuracy = Accuracy(task="binary")
+            self.f1 = F1Score(task="binary")
+            self.auroc = AUROC(task="binary")
+        elif dataset == 'dodh':
+            num_classes = 5
+            self.num_vertices = 16
+            # for DOD-H
+            self.macro_f1 = F1Score(task="multiclass", average="macro", num_classes=num_classes)
+            self.cohen_kappa = CohenKappa(task="multiclass", num_classes=num_classes)
+
         self.seq_pool_type = seq_pool_type
         self.vertex_pool_type = vertex_pool_type
         self.lr = lr
@@ -42,11 +57,7 @@ class LightGTMamba(L.LightningModule):
         self.edge_learner_attention = edge_learner_attention
         self.edge_learner_time_varying = edge_learner_time_varying
         self.pass_edges_to_next_layer = pass_edges_to_next_layer
-
-        # if FFT, input_dim = 100. Else, input_dim = 200.
-        print("Input dim: ", input_dim)
-        print("d_model: ", d_model)
-        self.in_proj = torch.nn.Linear(input_dim, d_model)     # from arshia's code
+        self.in_proj = torch.nn.Linear(input_dim, d_model)
         self.blocks = torch.nn.ModuleList([
             TGMamba(
                 d_model=d_model,
@@ -63,13 +74,8 @@ class LightGTMamba(L.LightningModule):
                 attn_softmax_temp=attn_softmax_temp,
             ) for _ in range(num_tgmamba_layers)
         ])
-        self.fc = torch.nn.Linear(d_model, 1)
-        torch.set_float32_matmul_precision('high') 
+        self.classifier = torch.nn.Linear(d_model, num_classes) # num_classes = 1 for tuhz or 5 for dodh
 
-        # Initialize metrics
-        self.accuracy = Accuracy(task="binary")
-        self.f1 = F1Score(task="binary")
-        self.auroc = AUROC(task="binary")
 
     def forward(self, data: Data):
         """
@@ -82,7 +88,7 @@ class LightGTMamba(L.LightningModule):
         batch, seqlen, _ = data.x.size()
         num_vertices = self.num_vertices
         batch = batch // num_vertices
-        # print("data.x.size(): ", data.x.size())
+        print("data.x.size(): ", data.x.size())
         out = self.in_proj(data.x)  # (B*V, L, d_model)
         assert not torch.isnan(out).any(), "NaN in input data"
         # out = data.x
@@ -123,25 +129,38 @@ class LightGTMamba(L.LightningModule):
     def training_step(self, data, batch_idx):
         out = self(data)
         assert out.size(0) == data.y.size(0), "Batch size mismatch"
-        assert out.size(1) == 1, "Output size mismatch: output has more than 1 feature"
         
-        # Compute metrics
-        preds = torch.sigmoid(out)
-        assert preds.size(0) == data.y.size(0), "Batch size mismatch"
-        targets = data.y.type(torch.float32).reshape(-1, 1)
-        loss = F.binary_cross_entropy_with_logits(out, targets)
-        accuracy = self.accuracy(preds, targets)
-        f1 = self.f1(preds, targets)
-        auroc = self.auroc(preds, targets)
+        if self.dataset == 'tuhz':
+            probs = torch.sigmoid(out)
+            preds = (probs > 0.5).type(torch.float32)
+            targets = data.y.type(torch.float32).reshape(-1, 1)
+            loss = F.binary_cross_entropy_with_logits(out, targets)
+            accuracy = self.accuracy(preds, targets)
+            f1 = self.f1(preds, targets)
+            auroc = self.auroc(probs, targets)
+            log_dict = {
+                "train/loss": loss,
+                "train/accuracy": accuracy,
+                "train/f1": f1,
+                "train/auroc": auroc,
+            }
+        elif self.dataset == 'dodh':
+            print("out.size(): ", out.size())
+            print("targets.size(): ", targets.size())
+            loss = F.cross_entropy(out, targets)
+            probs = torch.softmax(out, dim=-1)
+            preds = torch.argmax(probs, dim=-1)
+            macro_f1 = self.macro_f1(preds, targets)
+            cohen_kappa = self.cohen_kappa(preds, targets)
 
-        # Create log dictionary
-        log_dict = {
-            "train/loss": loss,
-            "train/accuracy": accuracy,
-            "train/f1": f1,
-            "train/auroc": auroc,
-        }
-
+            log_dict = {
+                "train/loss": loss,
+                "train/macro_f1": macro_f1,
+                "train/cohen_kappa": cohen_kappa,
+            }
+        else:
+            raise NotImplementedError
+        
         # Log all metrics at once
         self.log_dict(log_dict, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=data.num_graphs, sync_dist=True)
 
@@ -150,68 +169,113 @@ class LightGTMamba(L.LightningModule):
     def validation_step(self, data, batch_idx):
         out = self(data)
         assert out.size(0) == data.y.size(0), "Batch size mismatch"
-        assert out.size(1) == 1, "Output size mismatch: output has more than 1 feature"
 
-        # Compute metrics
-        preds = torch.sigmoid(out)
-        assert preds.size(0) == data.y.size(0), "Batch size mismatch"
-        targets = data.y.type(torch.float32).reshape(-1, 1)
-        loss = F.binary_cross_entropy_with_logits(out, targets)
-        accuracy = self.accuracy(preds, targets)
-        f1 = self.f1(preds, targets)
-        auroc = self.auroc(preds, targets)
+        if self.dataset == 'tuhz':
+            probs = torch.sigmoid(out)
+            preds = (probs > 0.5).type(torch.float32)
+            targets = data.y.type(torch.float32).reshape(-1, 1)
+            loss = F.binary_cross_entropy_with_logits(out, targets)
+            accuracy = self.accuracy(preds, targets)
+            f1 = self.f1(preds, targets)
+            auroc = self.auroc(probs, targets)
+            log_dict = {
+                "val/loss": loss,
+                "val/accuracy": accuracy,
+                "val/f1": f1,
+                "val/auroc": auroc,
+            }
+        elif self.dataset == 'dodh':
+            print("out.size(): ", out.size())
+            print("targets.size(): ", targets.size())
+            loss = F.cross_entropy(out, targets)
+            probs = torch.softmax(out, dim=-1)
+            preds = torch.argmax(probs, dim=-1)
+            macro_f1 = self.macro_f1(preds, targets)
+            cohen_kappa = self.cohen_kappa(preds, targets)
 
-        log_dict = {
-            "val/loss": loss,
-            "val/accuracy": accuracy,
-            "val/f1": f1,
-            "val/auroc": auroc,
-        }
-
+            log_dict = {
+                "val/loss": loss,
+                "val/macro_f1": macro_f1,
+                "val/cohen_kappa": cohen_kappa,
+            }
+        else:
+            raise NotImplementedError
+        
         self.log_dict(log_dict, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=data.num_graphs, sync_dist=True)
 
-        return {"loss": loss, "auroc": auroc}
+        return loss
 
     def test_step(self, data, batch_idx):
         out = self(data)
         assert out.size(0) == data.y.size(0), "Batch size mismatch"
-        assert out.size(1) == 1, "Output size mismatch: output has more than 1 feature"
         
-        preds = torch.sigmoid(out)
-        targets = data.y.type(torch.float32).reshape(-1, 1)
-        loss = F.binary_cross_entropy_with_logits(out, targets)
-        accuracy = self.accuracy(preds, targets)
-        f1 = self.f1(preds, targets)
-        auroc = self.auroc(preds, targets)
+        if self.dataset == 'tuhz':
+            probs = torch.sigmoid(out)
+            preds = (probs > 0.5).type(torch.float32)
+            targets = data.y.type(torch.float32).reshape(-1, 1)
+            loss = F.binary_cross_entropy_with_logits(out, targets)
+            accuracy = self.accuracy(preds, targets)
+            f1 = self.f1(preds, targets)
+            auroc = self.auroc(probs, targets)
+            log_dict = {
+                "test/loss": loss,
+                "test/accuracy": accuracy,
+                "test/f1": f1,
+                "test/auroc": auroc,
+            }
+        elif self.dataset == 'dodh':
+            print("out.size(): ", out.size())
+            print("targets.size(): ", targets.size())
+            loss = F.cross_entropy(out, targets)
+            probs = torch.softmax(out, dim=-1)
+            preds = torch.argmax(probs, dim=-1)
+            macro_f1 = self.macro_f1(preds, targets)
+            cohen_kappa = self.cohen_kappa(preds, targets)
 
-        log_dict = {
-            "test/loss": loss,
-            "test/accuracy": accuracy,
-            "test/f1": f1,
-            "test/auroc": auroc,
-        }
+            log_dict = {
+                "test/loss": loss,
+                "test/macro_f1": macro_f1,
+                "test/cohen_kappa": cohen_kappa,
+            }
+        else:
+            raise NotImplementedError
 
         self.log_dict(log_dict, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=data.num_graphs, sync_dist=True)
 
         return loss
     
     def on_train_epoch_end(self):
-        # Reset metrics at the end of each training epoch
-        self.accuracy.reset()
-        self.f1.reset()
-        self.auroc.reset()
-
+        if self.dataset == 'tuhz':
+            self.accuracy.reset()
+            self.f1.reset()
+            self.auroc.reset()
+        elif self.dataset == 'dodh':
+            self.macro_f1.reset()
+            self.cohen_kappa.reset()
+        else:
+            raise NotImplementedError
+        
     def on_validation_epoch_end(self):
-        # Reset metrics at the end of each validation epoch
-        self.accuracy.reset()
-        self.f1.reset()
-        self.auroc.reset()
+        if self.dataset == 'tuhz':
+            self.accuracy.reset()
+            self.f1.reset()
+            self.auroc.reset()
+        elif self.dataset == 'dodh':
+            self.macro_f1.reset()
+            self.cohen_kappa.reset()
+        else:
+            raise NotImplementedError
 
     def on_test_epoch_end(self):
-        # Reset metrics at the end of each test epoch
-        self.accuracy.reset()
-        self.f1.reset()
-        self.auroc.reset()
+        if self.dataset == 'tuhz':
+            self.accuracy.reset()
+            self.f1.reset()
+            self.auroc.reset()
+        elif self.dataset == 'dodh':
+            self.macro_f1.reset()
+            self.cohen_kappa.reset()
+        else:
+            raise NotImplementedError
     
     def configure_optimizers(self):
         if self.optimizer_name == "adam":
